7b342f1e0924a1ac53672ad5cb9c77ad48c99383
“kuokuanyo” <a167829435@gmail.com>
2019-08-23 17:56:38 +0800

    0823

--- 多元線性迴歸/多元線性迴歸.py
+++ 多元線性迴歸/多元線性迴歸.py
@@ -17,14 +17,79 @@ labelencoder_x = LabelEncoder()
 x[:, 3] = labelencoder_x.fit_transform(x[:, 3])
 
 #虛擬變數
-from sklearn.compose import ColumnTransformer
-transformer = ColumnTransformer([('one_hot_encoder',
-            OneHotEncoder(categories='auto'), [3])],remainder='passthrough')
-x = np.array(transformer.fit_transform(x), dtype=np.float)
-
-'''
-onehotencoder = OneHotEncoder(categorical_features = [3])
+onehotencoder = OneHotEncoder(categorical_features = [3],
+                              handle_unknown='ignore')
+#直接使用fit_transform()執行訓練及轉換
 x = onehotencoder.fit_transform(x).toarray()
-'''
+#刪除三欄虛擬變數中其中的一欄(避免共線性)
+#三個虛擬變數只能取兩欄
+x = x[:, 1:] #刪除第一欄
+
+#分測試訓練集
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size = 0.2, random_state = 0)
+
+#線性迴歸不需要進行特徵縮放
+#多元線性迴歸
+from sklearn.linear_model import LinearRegression
+#建立迴歸模型
+regressor = LinearRegression()
+#訓練迴歸模型
+regressor.fit(x_train, y_train)
+
+#預測結果
+y_pred = regressor.predict(x_test)
+
+#逐步迴歸(倒退消去)
+import statsmodels.api as sm #不包含常數項(必須新增一欄)
+#np.ones(40, 1) 建立40 * 1的矩陣(數值都為1)
+#axis = 1 欄
+x_train = np.append(arr = np.ones((40, 1)),
+                    values = x_train, axis = 1)
+
+#模型篩選
+#先將所有欄位加入
+x_opt = x_train[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = x_opt).fit()
+regressor_OLS.summary()
+
+#刪除 x2欄(p-value = 0.85)
+x_opt = np.delete(x_opt, 2, axis = 1)
+#也可以使用 x_opt = x_train[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = x_opt).fit()
+regressor_OLS.summary()
+
+#刪除 x1 欄
+x_opt = np.delete(x_opt, 1, axis = 1)
+#也可以使用 x_opt = x_train[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = x_opt).fit()
+regressor_OLS.summary()
+
+#刪除 x2 欄
+x_opt = np.delete(x_opt, 2, axis = 1)
+#也可以使用 x_opt = x_train[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y_train, exog = x_opt).fit()
+print(regressor_OLS.summary()) #都顯著
+
+#刪除 x2 欄
+x_opt = np.delete(x_opt, 2, axis = 1)
+#也可以使用 x_opt = x_train[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = y_train, exog = x_opt).fit()
+print(regressor_OLS.summary()) #都顯著
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ 多元線性迴歸/多元線性迴歸.r
@@ -0,0 +1,37 @@
+#多元線性迴歸
+#匯入資料
+data = read.csv('50_Startups.csv')
+
+#分類資料
+data$State = factor(data$State,
+                    levels = c('New York', 'California', 'Florida'),
+                    labels = c(1, 2,3))
+
+#分訓練及測試
+library(caTools)
+set.seed(123)
+split = sample.split(data$Profit, SplitRatio = 0.8)
+train_set = subset(data, split == TRUE)
+test_set = subset(data, split == FALSE)
+
+#不需要特徵縮放
+#建立迴歸模型
+#不需要刪除虛擬變數其中一欄，會自動執行，因此沒共線性問題
+attach(data)
+model = lm(Profit ~ ., data = train_set)
+summary(regressor)
+
+#預測測試集
+y_pred = predict(regressor, test_set)
+
+#篩選變數(倒退刪除法)
+#刪除State
+train_set = train_set[, -4]
+model = lm(Profit ~ ., data = train_set)
+summary(model)
+
+#刪除R.D.Spend欄(第一欄)
+train_set = train_set[, -1]
+#或使用train_set$R.D.Spend = NULL 刪除特定欄位
+model = lm(Profit ~ ., data = train_set)
+summary(model)
